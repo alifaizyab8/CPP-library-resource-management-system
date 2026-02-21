03296656dbaa2853977739fcac1ee33328e70c1f
alifaizyab8 <alifaizyab8@gmail.com>
2026-02-22 00:07:03 +0500

    New Constructor for New user

--- src/domain/User.h
+++ src/domain/User.h
@@ -24,6 +24,12 @@ public:
         : userId(id), username(uname), password(pass), firstName(fName), lastName(lName), email(email),
           address(addr), phone(phone), balance(bal), membershipTypeId(memTypeId), registrationDate(regDate), isActive(active) {}
 
+    // Added Second Constrcutor for New Users
+    User(const std::string &uname, const std::string &pass, const std::string &fName,
+         const std::string &lName, const std::string &email, const std::string &addr, const std::string &phone,
+         double bal, int memTypeId, const std::string &regDate, bool active)
+        : userId(0), username(uname), password(pass), firstName(fName), lastName(lName), email(email),
+          address(addr), phone(phone), balance(bal), membershipTypeId(memTypeId), registrationDate(regDate), isActive(active) {}
     int getUserId() const
     {
         return userId;
