647a7442ebea726a97435a34ff0c55ff9a55cb2d
Bareerah22 <bareerahkazmi02@gmail.com>
2026-02-26 21:03:54 +0500

    added ResourceRepository.h

--- src/infrastructure/repositories/ResourceRepository.h
+++ src/infrastructure/repositories/ResourceRepository.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <vector>
+#include <memory>
+#include <string>
+#include "sqlite3.h"
+#include "../../domain/Resource.h"
+
+class ResourceRepository {
+private:
+    sqlite3* db;
+    bool insertResource(Resource& resource);
+    bool updateResource(const Resource& resource);
+
+public:
+    explicit ResourceRepository(sqlite3* connection);
+    ~ResourceRepository();
+
+    bool save(Resource& resource);
+    bool deleteResource(int resourceId);
+    std::unique_ptr<Resource> getById(int resourceId);
+    std::vector<Resource> getAll();
+};
