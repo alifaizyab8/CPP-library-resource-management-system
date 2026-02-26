37e74987d40d774cd3770ef9382b8998cf50ff98
Bareerah22 <bareerahkazmi02@gmail.com>
2026-02-26 20:57:10 +0500

    added CategoryRepository.h

--- src/infrastructure/repositories/CategoryRepository.h
+++ src/infrastructure/repositories/CategoryRepository.h
@@ -1,3 +1,22 @@
 #pragma once
+#include <vector>
+#include <memory>
+#include <string>
+#include "sqlite3.h"
+#include "../../domain/Category.h"
 
-// resource type
+class CategoryRepository {
+private:
+    sqlite3* db;
+    bool insertCategory(Category& category);
+    bool updateCategory(const Category& category);
+
+public:
+    explicit CategoryRepository(sqlite3* connection);
+    ~CategoryRepository();
+
+    bool save(Category& category);
+    bool deleteCategory(int categoryId);
+    std::unique_ptr<Category> getById(int categoryId);
+    std::vector<Category> getAll();
+};
