26e4585a1c826c49f2fb829c392a9ab3cf654ed4
waqarwasif <waqarwasif0333@gmail.com>
2026-03-13 16:13:27 +0500

    UserRepository CPP file modification commit

--- src/infrastructure/repositories/UserRepository.cpp
+++ src/infrastructure/repositories/UserRepository.cpp
@@ -318,3 +318,50 @@ bool UserRepository::save(User &user)
     }
     return updateUser(user);
 }
+
+
+std::vector<User> UserRepository::getPendingDeletionRequests()
+{
+
+    std::vector<User> users;
+
+    const char *sql =
+        "SELECT user_id, username, password, first_name, last_name, email, "
+        "address, phone, balance, membership_type_id, registration_date, is_active, deletion_requested "
+        "FROM users WHERE deletion_requested=1;"; // Added the condition to only get users with deletion requested
+
+    sqlite3_stmt *stmt = nullptr;
+
+    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK)
+    {
+        cerr << "Failed to prepare SELECT ALL: " << sqlite3_errmsg(db) << endl;
+        return users;
+    }
+
+    auto safeText = [](sqlite3_stmt *stmt, int col) -> string
+    {
+        const unsigned char *text = sqlite3_column_text(stmt, col);
+        return text ? reinterpret_cast<const char *>(text) : "";
+    };
+
+    while (sqlite3_step(stmt) == SQLITE_ROW)
+    {
+        users.emplace_back(
+            sqlite3_column_int(stmt, 0),
+            safeText(stmt, 1),
+            safeText(stmt, 2),
+            safeText(stmt, 3),
+            safeText(stmt, 4),
+            safeText(stmt, 5),
+            safeText(stmt, 6),
+            safeText(stmt, 7),
+            sqlite3_column_double(stmt, 8),
+            sqlite3_column_int(stmt, 9),
+            safeText(stmt, 10),
+            sqlite3_column_int(stmt, 11) == 1,
+            sqlite3_column_int(stmt, 12) == 1); //  Added deletionRequested
+    }
+
+    sqlite3_finalize(stmt);
+    return users;
+}
