b30e91aeddbeeff89f0305fa61d954b0fd1c836e
waqarwasif <waqarwasif0333@gmail.com>
2026-03-14 16:03:27 +0500

    Date.cpp: New Function Added

--- src/Utility/date.cpp
+++ src/Utility/date.cpp
@@ -38,3 +38,29 @@ std::string getDueDate(int daysToAdd, std::string currentDate=getCurrentDate())
 
     return std::to_string(year) + "-" + std::to_string(month) + "-" + std::to_string(day);
 }
+
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+
+// Helper to calculate difference in days
+int calculateDaysOverdue(const std::string &dueDateStr, const std::string &todayStr)
+{
+    std::tm dueTm = {};
+    std::tm todayTm = {};
+
+    std::istringstream ssDue(dueDateStr);
+    std::istringstream ssToday(todayStr);
+
+    ssDue >> std::get_time(&dueTm, "%Y-%m-%d");
+    ssToday >> std::get_time(&todayTm, "%Y-%m-%d");
+
+    std::time_t dueTime = std::mktime(&dueTm);
+    std::time_t todayTime = std::mktime(&todayTm);
+
+    if (dueTime == -1 || todayTime == -1)
+        return 0; // Error parsing
+
+    double difference = std::difftime(todayTime, dueTime);
+    return difference / (60 * 60 * 24); // Convert seconds to days
+}
