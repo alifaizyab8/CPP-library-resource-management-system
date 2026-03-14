f26103c1421b7827d4c933de45cac03fcf24d93f
waqarwasif <waqarwasif0333@gmail.com>
2026-03-14 16:03:14 +0500

    Date.h: New Function Added

--- src/Utility/date.h
+++ src/Utility/date.h
@@ -3,3 +3,4 @@
 
 std::string getCurrentDate();
 std::string getDueDate(int daysToAdd = 14, std::string currentDate = getCurrentDate());
+int calculateDaysOverdue(const std::string &dueDateStr, const std::string &todayStr);
